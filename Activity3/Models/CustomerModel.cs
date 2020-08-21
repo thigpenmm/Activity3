0ff87538eb07e22d27dfee6d930762c353c442ba
Thigpen <mthigpen@amazon.com>
2020-08-20 19:31:51 -0500

    Add project files.

---
+++ Activity3/Models/CustomerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activity3.Models
+{
+    public class CustomerModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public CustomerModel(int iD, string name, int age)
+        {
+            ID = iD;
+            Name = name;
+            Age = age;
+        }
+    }
+}
